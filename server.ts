9fb35895acbd1473e3d9edc94e0b7ebccc84423e
Shivabhandari122 <qshiva906@gmail.com>
2025-02-04 21:32:00 +0545

    database connected successfully

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import app from "../backend/src/app";
 import envConfig from "./src/config/config";
+import connectToDatabase from "./src/config/db";
 
-const startServer = () => {
+const startServer = async () => {
+  await connectToDatabase();
   const port = envConfig.port || 3000;
   app.listen(port, () => {
     console.log(`server has started at port ${port}`);
--- src/config/db.ts
+++ src/config/db.ts
@@ -3,12 +3,14 @@ import envConfig from "./config";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(envConfig.mongodbString as string);
     mongoose.connection.on("connected", () => {
       console.log("Connected to db successfully");
     });
+    await mongoose.connect(envConfig.mongodbString as string);
   } catch (error) {
     console.log("Failed to connected to db!!!");
     process.exit(1); //this will forcefully exit program if something went wrong in db connection
   }
 };
+
+export default connectToDatabase;
